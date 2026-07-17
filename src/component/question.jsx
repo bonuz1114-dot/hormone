3f3c16e669c2e6c671eb60648511bce2446d18df
Chon <bonuz@gmail.com>
2026-07-17 20:54:44 +0700

    first commit

---
+++ src/component/question.jsx
@@ -0,0 +1,212 @@
+export const ALL_QUESTIONS = [
+  {
+    "id": 1,
+    "question": "ฮอร์โมนใดทำหน้าที่กระตุ้นการหลั่งฮอร์โมนเพศ (FSH และ LH) จากต่อมใต้สมอง?",
+    "options": ["GnRH", "TRH", "CRH", "PIH"],
+    "answer": 0
+  },
+  {
+    "id": 2,
+    "question": "ฮอร์โมนใดจากไฮโปทาลามัส ทำหน้าที่กระตุ้นการหลั่งฮอร์โมน TSH ที่ควบคุมต่อมไทรอยด์?",
+    "options": ["GnRH", "TRH", "CRH", "PIH"],
+    "answer": 1
+  },
+  {
+    "id": 3,
+    "question": "ฮอร์โมนใดทำหน้าที่กระตุ้นต่อมใต้สมองส่วนหน้าให้หลั่งฮอร์โมน ACTH เพื่อไปควบคุมต่อมหมวกไต?",
+    "options": ["GnRH", "TRH", "CRH", "PIH"],
+    "answer": 2
+  },
+  {
+    "id": 4,
+    "question": "ฮอร์โมนใดทำหน้าที่ยับยั้งการหลั่งฮอร์โมนโพรแลกติน (ฮอร์โมนสร้างน้ำนม)?",
+    "options": ["GHIH", "TRH", "GnRH", "PIH"],
+    "answer": 3
+  },
+  {
+    "id": 5,
+    "question": "ฮอร์โมนใดทำหน้าที่กระตุ้นการหลั่งฮอร์โมนการเจริญเติบโต (Growth Hormone)?",
+    "options": ["GHRH", "GHIH", "CRH", "TRH"],
+    "answer": 0
+  },
+  {
+    "id": 6,
+    "question": "ฮอร์โมนใดทำหน้าที่ยับยั้งการหลั่งฮอร์โมนการเจริญเติบโต (Growth Hormone)?",
+    "options": ["GHRH", "GHIH", "CRH", "TRH"],
+    "answer": 1
+  },
+  {
+    "id": 7,
+    "question": "ฮอร์โมนใดทำหน้าที่กระตุ้นการเจริญเติบโตของไข่ในเพศหญิง และกระตุ้นการสร้างอสุจิในเพศชาย?",
+    "options": ["LH", "ACTH", "FSH", "TSH"],
+    "answer": 2
+  },
+  {
+    "id": 8,
+    "question": "ฮอร์โมนใดมีบทบาทสำคัญที่สุดที่ทำให้เกิดการตกไข่ในเพศหญิง?",
+    "options": ["FSH", "ACTH", "TSH", "LH"],
+    "answer": 3
+  },
+  {
+    "id": 9,
+    "question": "ฮอร์โมนใดทำหน้าที่กระตุ้นการทำงานของต่อมหมวกไตส่วนนอกให้ผลิต Cortisol?",
+    "options": ["ACTH", "TSH", "FSH", "LH"],
+    "answer": 0
+  },
+  {
+    "id": 10,
+    "question": "ฮอร์โมนใดทำหน้าที่กระตุ้นให้ต่อมไทรอยด์สร้างและหลั่งฮอร์โมนไทรอกซิน?",
+    "options": ["ACTH", "TSH", "FSH", "LH"],
+    "answer": 1
+  },
+  {
+    "id": 11,
+    "question": "ฮอร์โมนใดทำหน้าที่กระตุ้นต่อมน้ำนมให้เกิดการสร้างน้ำนมในแม่ลูกอ่อน?",
+    "options": ["Oxytocin", "Estrogen", "Prolactin", "Progesterone"],
+    "answer": 2
+  },
+  {
+    "id": 12,
+    "question": "ฮอร์โมนแห่งความสุขชนิดใด ทำหน้าที่ช่วยระงับความเจ็บปวดในร่างกาย?",
+    "options": ["Melatonin", "Oxytocin", "Thymosin", "Endorphin"],
+    "answer": 3
+  },
+  {
+    "id": 13,
+    "question": "ฮอร์โมนใดสร้างจากต่อมไทรอยด์ ทำหน้าที่ควบคุมอัตราเมตาบอลิซึม (การเผาผลาญ) ของร่างกาย?",
+    "options": ["Thyroxine", "Calcitonin", "Insulin", "Glucagon"],
+    "answer": 0
+  },
+  {
+    "id": 14,
+    "question": "ฮอร์โมนใดหลั่งออกมาเมื่อร่างกายเกิดความเครียด ทำหน้าที่เพิ่มระดับน้ำตาลในเลือด?",
+    "options": ["Insulin", "Cortisol", "Calcitonin", "Melatonin"],
+    "answer": 1
+  },
+  {
+    "id": 15,
+    "question": "ฮอร์โมนใดหลั่งออกมามากในภาวะฉุกเฉิน เช่น ตกใจ ตื่นเต้น (ฮอร์โมนสู้หรือหนี)?",
+    "options": ["Insulin", "Thyroxine", "Epinephrine(Adrenaline)", "Glucagon"],
+    "answer": 2
+  },
+  {
+    "id": 16,
+    "question": "ฮอร์โมนใดหลั่งจากต่อมหมวกไตส่วนใน มีหน้าที่คล้ายอะดรีนาลีนและช่วยเพิ่มความดันโลหิต?",
+    "options": ["Epinephrine", "Cortisol", "Thyroxine", "Norepinephirne(Noradrenaline)"],
+    "answer": 3
+  },
+  {
+    "id": 17,
+    "question": "ฮอร์โมนใดทำหน้าที่ลดระดับน้ำตาลในเลือด?",
+    "options": ["insulin", "Glucagon", "Cortisol", "Epinephrine"],
+    "answer": 0
+  },
+  {
+    "id": 18,
+    "question": "ฮอร์โมนใดทำหน้าที่ 'เพิ่ม' ระดับน้ำตาลในเลือด โดยเปลี่ยนไกลโกเจนในตับให้เป็นกลูโคส?",
+    "options": ["insulin", "Glucagon", "Cortisol", "Thyroxine"],
+    "answer": 1
+  },
+  {
+    "id": 19,
+    "question": "ฮอร์โมนเพศหญิงชนิดใด ควบคุมการแสดงออกของลักษณะเพศหญิง เช่น สะโพกผาย เสียงแหลม?",
+    "options": ["Progesterone", "Testosterone", "Estrogen", "Androgen"],
+    "answer": 2
+  },
+  {
+    "id": 20,
+    "question": "ฮอร์โมนใดทำงานร่วมกับอีสโทรเจนในการเตรียมผนังมดลูกเพื่อรองรับการฝังตัวของตัวอ่อน?",
+    "options": ["Estrogen", "Testosterone", "Androgen", "Progesterone"],
+    "answer": 3
+  },
+  {
+    "id": 21,
+    "question": "ฮอร์โมนเพศชายที่สำคัญที่สุด ซึ่งทำหน้าที่ควบคุมลักษณะความเป็นชายคือฮอร์โมนใด?",
+    "options": ["Testoterone", "Estrogen", "Progesterone", "hCG"],
+    "answer": 0
+  },
+  {
+    "id": 22,
+    "question": "กลุ่มฮอร์โมนเพศชายที่กระตุ้นลักษณะความเป็นชาย มีชื่อเรียกรวม ๆ ว่ากลุ่มฮอร์โมนใด?",
+    "options": ["Estrogen", "Androgen", "Progesterone", "Glucocorticoids"],
+    "answer": 1
+  },
+  {
+    "id": 23,
+    "question": "ฮอร์โมนใดควบคุมความสูงและการเจริญเติบโตของกระดูกและกล้ามเนื้อในร่างกาย?",
+    "options": ["Thyroxine", "Insulin", "GH", "Cortisol"],
+    "answer": 2
+  },
+  {
+    "id": 24,
+    "question": "ฮอร์โมนใดที่ทำหน้าที่กระตุ้นการดูดกลับน้ำที่ท่อหน่วยไตคือฮอร์โมนใด?",
+    "options": ["Oxytocin", "hCG", "Thymosin", "ADH"],
+    "answer": 3
+  },
+  {
+    "id": 25,
+    "question": "ฮอร์โมนกลุ่มใดทำหน้าที่ควบคุมสมดุลของน้ำและแร่ธาตุ เช่น โซเดียม ในร่างกาย?",
+    "options": ["Mineralocorticoids", "Glucocorticoids", "Androgen", "Estrogen"],
+    "answer": 0
+  },
+  {
+    "id": 26,
+    "question": "ฮอร์โมนใดทำหน้าที่ลดระดับแคลเซียมในเลือด โดยนำแคลเซียมไปเก็บสะสมไว้ที่กระดูก?",
+    "options": ["Parathormone", "Calcitonin", "Thyroxine", "Insulin"],
+    "answer": 1
+  },
+  {
+    "id": 27,
+    "question": "ฮอร์โมนใดทำหน้าที่เพิ่มระดับแคลเซียมในเลือด เมื่อร่างกายมีแคลเซียมในเลือดต่ำ?",
+    "options": ["Calcitonin", "Thyroxine", "Parathormone", "Glucagon"],
+    "answer": 2
+  },
+  {
+    "id": 28,
+    "question": "ฮอร์โมนใดควบคุมการนอนหลับและการตื่น โดยจะหลั่งออกมามากในเวลากลางคืนหรือที่มืด?",
+    "options": ["Thymosin", "Gastrin", "Secretin", "Melatonin"],
+    "answer": 3
+  },
+  {
+    "id": 29,
+    "question": "ฮอร์โมนใดกระตุ้นการหดตัวของมดลูกขณะคลอด และกระตุ้นการหลั่งน้ำนมขณะที่ทารกดูดนม?",
+    "options": ["Oxytocin", "Prolactin", "Estrogen", "Progesterone"],
+    "answer": 0
+  },
+  {
+    "id": 30,
+    "question": "ฮอร์โมนใดที่สร้างจากรก และเป็นฮอร์โมนที่ใช้ตรวจในการทดสอบว่า 'ตั้งครรภ์' หรือไม่?",
+    "options": ["LH", "hCG", "FSH", "Estrogen"],
+    "answer": 1
+  },
+  {
+    "id": 31,
+    "question": "ฮอร์โมนใดสร้างจากต่อมไทมัส ทำหน้าที่ช่วยพัฒนาเม็ดเลือดขาวและระบบภูมิคุ้มกัน?",
+    "options": ["Melatonin", "Gastrin", "Thymosin", "Secretin"],
+    "answer": 2
+  },
+  {
+    "id": 32,
+    "question": "ฮอร์โมนใดสร้างจากเนื้อเยื่อกระเพาะอาหาร ทำหน้าที่กระตุ้นการหลั่งกรดในกระเพาะอาหาร?",
+    "options": ["Secretin", "CCK", "Insulin", "Gastrin"],
+    "answer": 3
+  },
+  {
+    "id": 33,
+    "question": "ฮอร์โมนใดจากลำไส้เล็ก ทำหน้าที่กระตุ้นตับอ่อนให้หลั่งสารที่มีฤทธิ์เป็นเบสเพื่อลดความเป็นกรดของอาหาร?",
+    "options": ["Secretin", "Gastrin", "CCK", "Glucagon"],
+    "answer": 0
+  },
+  {
+    "id": 34,
+    "question": "ฮอร์โมนใดจากลำไส้เล็ก ทำหน้าที่กระตุ้นการหดตัวของถุงน้ำดีให้หลั่งน้ำดีออกมาย่อยไขมัน?",
+    "options": ["Gastrin", "Cholecystokinin(CCK)", "Secretin", "Insulin"],
+    "answer": 1
+  },
+  {
+    "id": 35,
+    "question": "ฮอร์โมนใดสร้างจากไต ทำหน้าที่กระตุ้นไขกระดูกให้สร้างเม็ดเลือดแดง?",
+    "options": ["Thymosin", "Cortisol", "Erythopoietin(EPO)", "ADH"],
+    "answer": 2
+  }
+]
